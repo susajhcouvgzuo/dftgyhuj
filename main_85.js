c03ccf37491b56720912b443e3b2da6001dd0def
susajhcouvgzuo <131784759+susajhcouvgzuo@users.noreply.github.com>
2023-08-17 10:05:01 -0300

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,21 @@
+menuListArray = [ "Marguerita",                 
+ "Pizza de queijo","Pizza de calabresa","Pizza de frango","Pizza quatro queijos","Pizza portuquesa"]; //Adicione mais nomes de pizza
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menuListArray.length; i++)
+    {
+        htmldata=htmldata+ menuListArray[i] + '<br>'
+    }
+    //Dê um nome apropriado ao Id, como displayMenu 
+    document.getElementById("displayMenu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Dê um nome de id apropriado, como addItem
+	var item = document.getElementById("addItem").value;
+    //Use a função push() para colocar o item no menuListArray
+    menuListArray.push(item);
+}
+
